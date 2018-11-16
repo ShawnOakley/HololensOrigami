c82ba7e6ce56fac4667a66d541d9cc9217654b4b
Shawn Oakley <shawnoakley@gmail.com>
2018-11-15 20:48:05 -0500

    Adds gaze selection, gestures

---
+++ Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCommands : MonoBehaviour {
+
+    // Called by GazeGestureManager when the user performs a Select Gesture
+    void OnSelect()
+    {
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+}
