ee7f5762c73f1f9bddc637a0492804fa0e348e5e
Shawn Oakley <shawnoakley@gmail.com>
2018-11-16 00:21:53 -0500

    Adds sound effects, verbal commands

---
+++ Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour {
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+	// Use this for initialization
+	void Start () {
+        keywords.Add("Reset world", () =>
+        {
+            this.BroadcastMessage("OnReset");
+        });
+
+        keywords.Add("Drop Sphere", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                focusObject.SendMessage("OnDrop", SendMessageOptions.DontRequireReceiver);
+            }
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
